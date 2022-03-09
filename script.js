6aa2dc886278533fc17f16ddd12ccd7f6b50d053
arhuny <91968435+arhuny@users.noreply.github.com>
2022-03-09 14:27:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var s = "Skill Safari"
+console.log(s.length)
+
+console.log(s.toUpperCase())
+
+console.log(s.indexOf("a"))
+
+console.log("Tony Stark/Iron Man: Stand down, final warning.\nSteve Rogers/Captain America: I could do this all day.")
+
+var weight = 120
+var height = 5
+
+let bmi = weight / (height * height)
+
+console.log(bmi)
